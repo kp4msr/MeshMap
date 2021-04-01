435cef8347fbbed153e74c7d6439c612a29c9d40
Manuel Santamaria <>
2021-04-01 09:39:29 -0400

    Adding compiled files

---
+++ build/precache-manifest.8ba3b055851c5f73d2e0c8da389b75ff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a35d9356d5a467e731083fb5d90534b7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3aa4ef7d1c8f5cc49c0f",
+    "url": "./static/css/2.42348699.chunk.css"
+  },
+  {
+    "revision": "c8ccfc38d74c6277b01b",
+    "url": "./static/css/main.653673f3.chunk.css"
+  },
+  {
+    "revision": "3aa4ef7d1c8f5cc49c0f",
+    "url": "./static/js/2.ba5edfd2.chunk.js"
+  },
+  {
+    "revision": "6791e7fb9584d65ed87d655035c7da95",
+    "url": "./static/js/2.ba5edfd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8ccfc38d74c6277b01b",
+    "url": "./static/js/main.b9ee12b0.chunk.js"
+  },
+  {
+    "revision": "4d179ce5f9368cd65785",
+    "url": "./static/js/runtime-main.3220e4b0.js"
+  },
+  {
+    "revision": "141da21b25dfa77a3667af85c13dd176",
+    "url": "./static/media/praredn.141da21b.png"
+  }
+]);
